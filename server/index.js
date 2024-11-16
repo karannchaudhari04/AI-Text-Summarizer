c830379f3e1cd4751327c2dd7af9dfe344372a80
karannchaudhari04 <karanchaudhari34804@gmail.com>
2024-11-16 20:44:18 +0530

    inintial commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 const summarizeText = require('./summarize.js');
 
@@ -7,7 +8,7 @@ const summarizeText = require('./summarize.js');
 app.use(express.json());
 
 // Serves static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Handle POST requests to the '/summarize' endpoint
 
--- server/summarize.js
+++ server/summarize.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+require('dotenv').config({ path: '../.env' });
 //This is the function where the call to the API is made. Returns the summarized text as a string.
 
 async function summarizeText(text) {
@@ -12,7 +12,6 @@ async function summarizeText(text) {
       "min_length": 30
     }
   });
-  require('dotenv').config();
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
